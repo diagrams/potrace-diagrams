4f895cf34d00c8e1af9215dd0f9bd83d40d29eb0
Christopher Chalmers <c.chalmers@me.com>
2015-05-25 16:09:49 +0100

    Update alphaMax example

--- examples/alphaMax.hs
+++ examples/alphaMax.hs
@@ -1,20 +1,27 @@
-import Diagrams.Backend.SVG
+import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Prelude
 import Diagrams.Potrace
-import System.IO.Unsafe
 import Codec.Picture
 
-bitmap :: Bitmap
-bitmap = unsafePerformIO $ do
+bitmap :: IO Bitmap
+bitmap = do
   Right png <- readPng "examples/D.png"
   return $ lumaThreshold png 0.5
 
-withAMax :: Double -> Path V2 Double
-withAMax a = tracePath' (with & alphaMax .~ a) bitmap
+withAMax :: Bitmap -> Double -> Path V2 Double
+withAMax bm a = tracePath' (with & alphaMax .~ a) bm
 
-example :: Diagram B
-example = frame 10 . fc black . stroke . hsep 10 $ map withAMax [0,0.3,0.5,1,1.3]
+alphas :: [Double]
+alphas =  [0,0.3,0.5,1,1.3]
 
-make :: Diagram B -> IO ()
-make = renderSVG "examples/alphaMax.svg" (mkWidth 1200)
+ds :: IO (Diagram B)
+ds = bitmap <&> \bm ->
+  map (withAMax bm) alphas
+    # hsep 10
+    # stroke
+    # fc black
+    # frame 10
+
+main :: IO ()
+main = mainWith ds
 
