7db831d72d6cc82d7fc0fb46f1b4cc5bbb157f88
Christopher Chalmers <c.chalmers@me.com>
2015-06-10 20:09:17 +0100

    Generalise to OrderedField

--- src/Diagrams/Potrace.hs
+++ src/Diagrams/Potrace.hs
@@ -43,39 +43,28 @@ import Graphics.Potrace as P
 
 -- | Trace the bitmap image to a path using potrace with 'def'
 --   parameters.
-tracePath :: Bitmap -> Path V2 Double
+tracePath :: OrderedField n => Bitmap -> Path V2 n
 tracePath = tracePath' def
 
 -- | Trace the bitmap image to a path curves using potrace with given
 --   parameters.
-tracePath' :: Parameters -> Bitmap -> Path V2 Double
+tracePath' :: OrderedField n => Parameters -> Bitmap -> Path V2 n
 tracePath' p bm = Path $ map (mapLoc Trail . loop) (trace' p bm)
 
 -- Internals -----------------------------------------------------------
 
-pt :: P.P2 -> D.P2 Double
-pt (P2 x y) = mkP2 x y
--- pt (P2 x y) = mkP2 (realToFrac x) (realToFrac y)
+-- | Convert a potrace point to a diagrams one.
+pt :: Fractional n => P.P2 -> D.P2 n
+pt (P2 x y) = mkP2 (realToFrac x) (realToFrac y)
+{-# INLINE pt #-}
 
-loop :: Curve -> Located (Trail' Loop V2 Double)
+-- | Convert a potrace curve to a diagrams loop.
+loop :: OrderedField n => Curve -> Located (Trail' Loop V2 n)
 loop (P.Curve p0 segs) = closeLine (lineFromSegments (go p0' segs)) `at` p0'
   where
     p0' = pt p0
-    -- for some reason the starting point is actaully the very last point.
     go p (s:ss) = case s of
       P.Corner v b   -> straight (pt v .-. p) : straight (pt b .-. pt v) : go (pt b) ss
       P.Bezier u v b -> bezier3 (pt u .-. p) (pt v .-. p) (pt b .-. p) : go (pt b) ss
     go _ []     = []
-
--- path :: [[P.Segment]] -> Path V2 Double
--- path = Path . map (mapLoc Trail . loop)
-
--- dia :: Renderable (Path V2 Double) b => P.Bitmap -> QDiagram b V2 Double Any
--- dia = stroke . path . P.pic
-
--- loop :: Curve (Trail' Loop V2 Double)
--- loop = foldCurve mkP2 corner cubic (, mempty) <&> \(p, ss) ->
---   where
---     corner u v (p,r)  = (p, straight (pt v .-. p) <| straight (pt b .-. pt v) <| r)
---     cubic u v b (p,r) = (p, bezier3 (pt u .-. p) (pt v .-. p) (pt b .-. p) <| go (pt b) ss)
 
