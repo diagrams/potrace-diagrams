e9fd995324042ba4acd624e1ec8259d25f78f7a7
Christopher Chalmers <c.chalmers@me.com>
2018-10-16 02:36:02 +0100

    Build with diagrams-2.0

--- src/Diagrams/Potrace.hs
+++ src/Diagrams/Potrace.hs
@@ -3,7 +3,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Diagarms.Potrace
--- Copyright   :  (c) 2015 Christopher Chalmers
+-- Copyright   :  (c) 2015-2018 Christopher Chalmers
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  c.chalmers@me.com
 --
@@ -38,7 +38,9 @@ module Diagrams.Potrace
   )
  where
 
-import Diagrams.Prelude as D
+import Data.Default
+import Data.Sequence
+import Geometry as G
 import Graphics.Potrace as P
 
 -- | Trace the bitmap image to a path using potrace with 'def'
@@ -49,17 +51,17 @@ tracePath = tracePath' def
 -- | Trace the bitmap image to a path curves using potrace with given
 --   parameters.
 tracePath' :: OrderedField n => Parameters -> Bitmap -> Path V2 n
-tracePath' p bm = Path $ map (mapLoc Trail . loop) (trace' p bm)
+tracePath' p bm = Path . fromList $ map (mapLoc ClosedTrail . loop) (trace' p bm)
 
 -- Internals -----------------------------------------------------------
 
 -- | Convert a potrace point to a diagrams one.
-pt :: Fractional n => P.P2 -> D.P2 n
-pt (P2 x y) = mkP2 (realToFrac x) (realToFrac y)
+pt :: Fractional n => P.P2 -> G.P2 n
+pt (P.P2 x y) = mkP2 (realToFrac x) (realToFrac y)
 {-# INLINE pt #-}
 
 -- | Convert a potrace curve to a diagrams loop.
-loop :: OrderedField n => Curve -> Located (Trail' Loop V2 n)
+loop :: OrderedField n => Curve -> Located (Loop V2 n)
 loop (P.Curve p0 segs) = closeLine (lineFromSegments (go p0' segs)) `at` p0'
   where
     p0' = pt p0
@@ -67,4 +69,3 @@ loop (P.Curve p0 segs) = closeLine (lineFromSegments (go p0' segs)) `at` p0'
       P.Corner v b   -> straight (pt v .-. p) : straight (pt b .-. pt v) : go (pt b) ss
       P.Bezier u v b -> bezier3 (pt u .-. p) (pt v .-. p) (pt b .-. p) : go (pt b) ss
     go _ []     = []
-
